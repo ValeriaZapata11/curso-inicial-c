e2fc4f0550bc52a7546bdac4339bf1d82627efb4
valeria zapata <zapatavaleria100@gmail.com>
2023-11-15 18:04:18 -0500

    otro tipo de operacion

---
+++ seccion 2/otro tipo de operador.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main () 
+{ 
+
+  int a=12, b= 7, suma=0, resta=0, multi=0, mod=0;
+  
+   float divi=0;
+   
+   suma= a+b;
+   resta=a-b;
+   musssssslti=a*b;
+   divi=a/(float)b;
+   mod=a%b;
+   
+   cout<<"la suma es:" <<suma<<"\n";
+   cout<<"la resta es:" <<resta<<"\n";
+   cout<<"la multiplicacion es:" <<multi<<"\n";
+   cout<<"la divicion es:" <<divi<<"\n";
+   cout<<"el restante es:" <<mod<<"\n";
+       
+       
+ return 0;
+}
